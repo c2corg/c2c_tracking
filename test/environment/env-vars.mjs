263c94c7c1f0ec926af5df05f40d08b1e6414db6
Bruno Besson <bruno.besson@viseo.com>
2022-09-29 08:35:45 +0200

    add missing test envvars

--- test/environment/env-vars.mjs
+++ test/environment/env-vars.mjs
@@ -1,6 +1,8 @@
 process.env['SERVER_BASE_URL'] = 'http://localhost:3000/';
 process.env['JWT_SECRET_KEY'] = 'secret';
+process.env['STRAVA_CLIENT_SECRET'] = 'd37d09886c3a92ced03feca580ccecd5630559ec';
 process.env['STRAVA_WEBHOOK_SUBSCRIPTION_VERIFY_TOKEN'] = '%trongpAssM0rd';
 process.env['SUUNTO_CLIENT_SECRET'] = 'secret';
+process.env['SUUNTO_SUBSCRIPTION_KEY'] = '4b24a2b497479fb5319543fdff28a495';
 process.env['SUUNTO_WEBHOOK_SUBSCRIPTION_TOKEN'] = '2fbbd34d-4dc3-44fc-8a47-9ba1bc037d2c';
 process.env['GARMIN_CONSUMER_SECRET'] = 'secret';
