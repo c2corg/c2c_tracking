f35718304839d1e15c2f186544bf4094113a7585
Bruno Besson <bruno.besson@gmail.com>
2022-10-19 14:54:23 +0200

    fix: health isn't exposing shuttingDown anymore

--- test/unit/health.service.spec.ts
+++ test/unit/health.service.spec.ts
@@ -8,18 +8,10 @@ describe('Health service', () => {
     timers.setSystemTime(new Date('2022-01-23'));
     expect(service.getStatus()).toMatchInlineSnapshot(`
       {
-        "isShuttingDown": false,
         "startTime": "2022-01-01T00:00:00.000Z",
         "upTime": "22 days",
       }
     `);
     jest.useRealTimers();
-  });
-
-  it('updates shutting down status', () => {
-    const service = new HealthService();
-    expect(service.getStatus()).toMatchObject({ isShuttingDown: false });
-    service.setShuttingDown();
-    expect(service.getStatus()).toMatchObject({ isShuttingDown: true });
   });
 });
