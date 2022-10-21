d944b54cc11f25c176c1269b56dddfdbc22a134a
Bruno Besson <bruno.besson@viseo.com>
2022-10-21 15:02:06 +0200

    feat: allow standard bearer auth

--- test/unit/auth/c2c-jwt-extractor.spec.ts
+++ test/unit/auth/c2c-jwt-extractor.spec.ts
@@ -3,7 +3,7 @@ import { createRequest } from 'node-mocks-http';
 import c2cJwtExtractor from '../../../src/auth/c2c-jwt-extractor';
 
 describe('c2cJwtExtractor', () => {
-  it('retrieves token from header', () => {
+  it('retrieves token from header (v6_api way)', () => {
     const req = createRequest({
       headers: {
         authorization: 'JWT token="a-nice-jwt-token"',
@@ -12,10 +12,19 @@ describe('c2cJwtExtractor', () => {
     expect(c2cJwtExtractor(req)).toBe('a-nice-jwt-token');
   });
 
+  it('retrieves token from header (bearer)', () => {
+    const req = createRequest({
+      headers: {
+        authorization: 'Bearer a-nice-jwt-token',
+      },
+    });
+    expect(c2cJwtExtractor(req)).toBe('a-nice-jwt-token');
+  });
+
   it('ignores invalid auth header', () => {
     const req = createRequest({
       headers: {
-        authorization: 'Bearer a-nice-jwt-token"',
+        authorization: 'Basic a-nice-jwt-token"',
       },
     });
     expect(c2cJwtExtractor(req)).toBeNull();
