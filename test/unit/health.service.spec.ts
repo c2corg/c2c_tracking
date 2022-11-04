b4b6e13282831189e9cb5e51c32a33aa4fe31104
Bruno Besson <bruno.besson@viseo.com>
2022-11-04 11:48:57 +0100

    feat: add version information in health endpoint

--- test/unit/health.service.spec.ts
+++ test/unit/health.service.spec.ts
@@ -6,12 +6,21 @@ describe('Health service', () => {
     timers.setSystemTime(new Date('2022-01-01'));
     const service = new HealthService();
     timers.setSystemTime(new Date('2022-01-23'));
-    expect(service.getStatus()).toMatchInlineSnapshot(`
+    expect(service.getStatus()).toMatchInlineSnapshot(
+      {
+        version: expect.stringMatching(
+          // eslint-disable-next-line security/detect-unsafe-regex
+          /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/,
+        ),
+      },
+      `
       {
         "startTime": "2022-01-01T00:00:00.000Z",
         "upTime": "22 days",
+        "version": StringMatching /\\^\\(0\\|\\[1-9\\]\\\\d\\*\\)\\\\\\.\\(0\\|\\[1-9\\]\\\\d\\*\\)\\\\\\.\\(0\\|\\[1-9\\]\\\\d\\*\\)\\(\\?:-\\(\\(\\?:0\\|\\[1-9\\]\\\\d\\*\\|\\\\d\\*\\[a-zA-Z-\\]\\[0-9a-zA-Z-\\]\\*\\)\\(\\?:\\\\\\.\\(\\?:0\\|\\[1-9\\]\\\\d\\*\\|\\\\d\\*\\[a-zA-Z-\\]\\[0-9a-zA-Z-\\]\\*\\)\\)\\*\\)\\)\\?\\(\\?:\\\\\\+\\(\\[0-9a-zA-Z-\\]\\+\\(\\?:\\\\\\.\\[0-9a-zA-Z-\\]\\+\\)\\*\\)\\)\\?\\$/,
       }
-    `);
+    `,
+    );
     jest.useRealTimers();
   });
 });
