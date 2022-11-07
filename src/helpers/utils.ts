94fad97b8f7231258f1115e6e4926e802a8ecd85
Bruno Besson <bruno.besson@viseo.com>
2022-11-07 13:10:45 +0100

    fix: handle npm version being 'dev' for tests

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,1 +1,5 @@
 export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
+
+export const semverRegex =
+  // eslint-disable-next-line security/detect-unsafe-regex
+  /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
